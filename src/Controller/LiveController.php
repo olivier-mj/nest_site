b3a4864e39531d0e02852ffc333b9690318234f6
Olivier Marie-Joseph <contact@oliviermariejoseph.fr>
2022-05-22 01:38:13 +0200

    update live component and twitchclient api

--- src/Controller/LiveController.php
+++ src/Controller/LiveController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Services\Twitch\TwitchClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LiveController extends AbstractController
 {
+    protected TwitchClient $client;
+
+    public function __construct(TwitchClient $client)
+    {
+        $this->client = $client;
+    }
+
     #[Route('/live', name: 'page.live')]
     public function index(): Response
     {
-        return $this->render('live/index.html.twig', [
-            'controller_name' => 'LiveController',
+
+        $replay  = $this->client->getReplay();
+
+        $response = $this->render('page/live.html.twig', [
+            'data' => $replay['data'],
+            'pagination' => $replay['pagination']
         ]);
+
+        return $response;
     }
 }
---
+++ src/Services/Twitch/TwitchClient.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Services\Twitch;
+
+use App\Services\Cache;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class TwitchClient
+{
+    private string $clientID;
+    private string $clientSecret;
+    private HttpClientInterface $client;
+    private Cache $cache;
+
+    public function __construct(
+        string $clientID,
+        string $clientSecret,
+        HttpClientInterface $client,
+        Cache $cache,
+    ) {
+        $this->clientID = $clientID;
+        $this->clientSecret = $clientSecret;
+        $this->client = $client;
+        $this->cache = $cache;
+    }
+
+    public function getLive(string $streamerName): array
+    {
+
+        $token = $this->getToken();
+
+
+        return $this->api('https://api.twitch.tv/helix/streams', 'GET', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token['access_token'],
+                'Client-Id' => $this->clientID
+            ],
+            'query' => [
+                'user_login' => $streamerName
+            ],
+        ]);
+    }
+
+
+    public function getReplay()
+    {
+        $token = $this->getToken();
+
+        return $this->cache->get('replay', function (ItemInterface $item) use ($token) {
+
+            $item->tag(['replay_list']);
+            return $this->api('https://api.twitch.tv/helix/videos', 'GET', [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $token['access_token'],
+                    'Client-Id' => $this->clientID
+                ],
+                'query' => [
+                    'user_id' =>  '150551018'
+                ]
+            ]);
+        });
+    }
+
+    private function api(string $endpoint, string $method = 'GET', array $options = []): array
+    {
+
+        $response = $this->client->request($method, $endpoint, $options);
+
+        if ($response->getStatusCode() >= 200 && $response->getStatusCode() < 300) {
+            return $response->toArray();
+        }
+        throw new TwitchException($response);
+    }
+
+    private function getToken(): array
+    {
+
+        $options = [
+            'query' => [
+                'client_id' => $this->clientID,
+                'client_secret' => $this->clientSecret,
+                'grant_type' => 'client_credentials'
+            ],
+            'http_version' => '2.0',
+        ];
+
+        $endpoint = "https://id.twitch.tv/oauth2/token";
+
+        return $this->cache->get('getToken', function (ItemInterface $item) use ($endpoint, $options) {
+            $item->expiresAfter(3600);
+            return $this->api($endpoint, 'POST', $options);
+        });
+    }
+}
